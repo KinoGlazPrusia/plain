892d83ce67f23bce898800e74e41704abbc32335
Josep <jmartinbolet@gmail.com>
2024-06-13 17:28:28 +0200

    Completed first functional version

--- src/PlainContext/PlainContext.js
+++ src/PlainContext/PlainContext.js
@@ -1,10 +1,20 @@
 export default class PlainContext {
+    static instances = {}
+
     constructor(name, self) { // 'self' hace referencia al componente que usa el contexto
         this.name = `${name}Context`
+        const context = PlainContext.instances[this.name]
+        if (context) {
+            context.subscribe(self)
+            return context
+        }
+
         this.subscribers = []
 
         this.initialise()
         this.subscribe(self)
+
+        PlainContext.instances[this.name] = this
     }
 
     initialise() {
@@ -19,17 +29,20 @@ export default class PlainContext {
         }
     }
 
-    setData(data) {
+    setData(data, propagate=false) {
         // Data must be a JSON object
         let context = JSON.parse(sessionStorage.getItem(this.name))
         if (!context) return
 
         context = {...context, ...data}
         sessionStorage.setItem(this.name, JSON.stringify(context))
+
+        // Context will propagate and re-render to all subscribed components
+        propagate ? this.propagate(): null;
     }
 
     getData(key) {
-        return JSON.parse(sessionStorage.getItem(this.name))[key]
+        return JSON.parse(sessionStorage.getItem(this.name))[key] 
     }
 
     propagate() {
@@ -40,8 +53,6 @@ export default class PlainContext {
 
     clear() {
         sessionStorage.removeItem(this.name)
+        delete PlainContext.instances[this.name]
     }
-
 }
-
-// Convertirlo a patrón singleton ¿?
