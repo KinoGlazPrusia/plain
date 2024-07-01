ce706cadd025160c789d4b709900d98e8782a7b2
Josep <jmartinbolet@gmail.com>
2024-07-01 13:53:15 +0200

    Updated

--- test/index.js
+++
@@ -1,8 +0,0 @@
-import myComp1 from "./myComp1/myComp1.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-    const a = document.getElementById('a')
-    const b = document.getElementById('b')
-
-    b.signals.connect(a, 'clicked', () => console.log("Hola"))
-})
