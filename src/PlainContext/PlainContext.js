636a32996cce59f4e8a1cb996f80ba8a984a6096
KinoGlazPrusia <jmartinbolet@gmail.com>
2025-02-21 10:40:49 +0100

    feat: add location selection to the contexts

--- src/PlainContext/PlainContext.js
+++ src/PlainContext/PlainContext.js
@@ -1,7 +1,7 @@
 export default class PlainContext {
     static instances = {}
 
-    constructor(contextName, attachedComponent, suscribe = true) { // 'attachedComponent' hace referencia al componente que usa el contexto
+    constructor(contextName, attachedComponent, suscribe = true, location='session') { // 'attachedComponent' hace referencia al componente que usa el contexto
         this.name = `${contextName}Context`
         const context = PlainContext.instances[this.name]
         if (context) {
@@ -12,6 +12,7 @@ export default class PlainContext {
         }
 
         this.subscribers = []
+        this.location = location
 
         this.#initialise()
         suscribe && this.subscribe(attachedComponent)
@@ -20,9 +21,17 @@ export default class PlainContext {
     }
 
     #initialise() {
-        sessionStorage.getItem(this.name)
-            ? null
-            : sessionStorage.setItem(this.name, JSON.stringify({}))
+        if (this.location === 'session') {
+            sessionStorage.getItem(this.name)
+                ? null
+                : sessionStorage.setItem(this.name, JSON.stringify({}))
+        }
+
+        else if (this.location === 'local') {
+            localStorage.getItem(this.name)
+                ? null
+                : localStorage.setItem(this.name, JSON.stringify({}))
+        }
     }
 
     #propagate() {
@@ -38,23 +47,43 @@ export default class PlainContext {
     }
 
     setData(data, propagate=false) {
-        // Data must be a JSON object
-        let context = JSON.parse(sessionStorage.getItem(this.name))
-        if (!context) return
+        if (this.location === 'session') {
+            // Data must be a JSON object
+            let context = JSON.parse(sessionStorage.getItem(this.name))
+            if (!context) return
 
-        context = {...context, ...data}
-        sessionStorage.setItem(this.name, JSON.stringify(context))
+            context = {...context, ...data}
+            sessionStorage.setItem(this.name, JSON.stringify(context))
+        } 
+        
+        else if (this.location === 'local') {
+            // Data must be a JSON object
+            let context = JSON.parse(localStorage.getItem(this.name))
+            if (!context) return
+
+            context = {...context, ...data}
+            localStorage.setItem(this.name, JSON.stringify(context))
+        }
 
         // Context will propagate and re-render to all subscribed components
         propagate && this.#propagate()
     }
 
     getData(key) {
-        return JSON.parse(sessionStorage.getItem(this.name))[key] 
+        return this.location === 'session' 
+            ? JSON.parse(sessionStorage.getItem(this.name))[key]
+            : JSON.parse(localStorage.getItem(this.name))[key]
     }
 
     clear() {
-        sessionStorage.removeItem(this.name)
+        if (this.location === 'session') {
+            sessionStorage.removeItem(this.name)
+        }
+
+        else if (this.location === 'local') {
+            localStorage.removeItem(this.name)
+        }
+
         delete PlainContext.instances[this.name]
     }
 }
