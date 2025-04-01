16d80ae88e8f3326fa9cd99b6e446b90b3134c2e
Josep <jmartinbolet@gmail.com>
2025-04-01 21:31:03 +0200

    refactor: import new class PlainStyle for automated style loading

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import PlainContext from "./PlainContext/PlainContext.js"
 import PlainSignal from "./PlainSignal/PlainSignal.js"
 import PlainState from "./PlainState/PlainState.js"
 import PlainRouter from "./PlainRouter/PlainRouter.js"
+import PlainStyle from "./PlainStyle/PlainStyle.js"
 
 export {
     PlainComponent,
     PlainContext,
     PlainSignal,
     PlainState,
-    PlainRouter
+    PlainRouter,
+    PlainStyle,
 }
 
 
